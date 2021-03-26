050ee9f74cba829011c9818fb00a5c595ab6a783
Jimmy <slimjim49j@gmail.com>
2021-03-26 16:11:57 -0600

    Change title, add cta build in animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { pages } from "./content.json";
 
@@ -6,24 +6,32 @@ import Marquee from "./components/marquee/marquee.js";
 import Header from "./components/header/header";
 
 function App() {
+  const [initialLoad, setInitialLoad] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setInitialLoad(false), 1100);
+  }, []);
+
   return (
-    <>
     <Router>
-      <Header pages={pages.map(({title, slug}) => ({title, slug })) } />
-      <Switch>
-        {
-          pages.map(page => (
-            <Route path={`/${page.slug}`} key={page.title}>
-              <Marquee title={page.title} {...page.blocks[0]} />
-            </Route>
-          ))
-        }
-        <Route exact path="/">
-          <Redirect to={`/${pages[0].slug}`} />
-        </Route>
-      </Switch>
+      <div className={initialLoad ? "initial-load" : ""}>
+        <Header pages={pages.map(({ title, slug }) => ({ title, slug }))} />
+        <Switch>
+          {
+            pages.map(page => (
+              <Route path={`/${page.slug}`} key={page.title}>
+                <Marquee
+                  title={page.title}
+                  {...page.blocks[0]}
+                />
+              </Route>
+            ))
+          }
+          <Route exact path="/">
+            <Redirect to={`/${pages[0].slug}`} />
+          </Route>
+        </Switch>
+      </div>
     </Router>
-    </>
   );
 }
 
