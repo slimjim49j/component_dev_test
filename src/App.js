49ef9f7d271c985ecb7e30f70ee53bc84000b9a6
Jimmy <slimjim49j@gmail.com>
2021-03-26 09:44:00 -0600

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import { pages } from "./content.json";
 
 import Marquee from "./components/marquee/marquee.js";
